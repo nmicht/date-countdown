14e7482677ccac08cfeb8972801685db23137c4f
Mihelle Torres <lic.nancy.torres@gmail.com>
2020-10-15 22:32:59 +0200

    Updates

---
+++ precache-manifest.3122f46e2bbf96d84311a8c02ae7e842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716ab6ee3d150078701952d60f8ce50b",
+    "url": "/date-countdown/index.html"
+  },
+  {
+    "revision": "762678067b66e7f8042a",
+    "url": "/date-countdown/static/css/main.d642f46b.chunk.css"
+  },
+  {
+    "revision": "cbddeab7d814c5d14aa2",
+    "url": "/date-countdown/static/js/2.ed125597.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/date-countdown/static/js/2.ed125597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762678067b66e7f8042a",
+    "url": "/date-countdown/static/js/main.5f1b0ce1.chunk.js"
+  },
+  {
+    "revision": "e753a92a8eb21e41d527",
+    "url": "/date-countdown/static/js/runtime-main.21ff7293.js"
+  }
+]);
